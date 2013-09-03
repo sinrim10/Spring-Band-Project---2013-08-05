fbf662cfde291a5c1b3b845813b808e47912c446
sinrim10 <sinrim10@naver.com>
2013-09-02 22:25:28 -0700

    Controller

---
+++ com.study.chap01/src/main/java/com/study/chap01/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.study.chap01.controller;
+
+public class Controller {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
