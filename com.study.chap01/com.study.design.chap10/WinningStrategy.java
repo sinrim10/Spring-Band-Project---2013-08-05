704af624993ac79d58eb884d7fb5ec7db03cccd9
sinrim10 <sinrim10@naver.com>
2013-09-10 22:03:13 -0700

    chap10

---
+++ com.study.chap01/com.study.design.chap10/WinningStrategy.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+
+public class WinningStrategy implements Strategy{
+	private Random random;
+	private boolean won = false;
+	private Hand prevHand;
+	public WinningStrategy(int seed) {
+		// TODO Auto-generated constructor stub
+		random = new Random(seed);
+	}
+	@Override
+	public Hand nextHand() {
+		// TODO Auto-generated method stub
+		if(!won){
+			prevHand = Hand.getHand(random.nextInt(3));
+		}
+		return prevHand;
+	}
+
+	@Override
+	public void study(boolean win) {
+		// TODO Auto-generated method stub
+		won = win;
+	}
+
+}
--- com.study.chap01/com.study.disign.chap08/Main3.java
+++ com.study.chap01/com.study.disign.chap08/Main3.java
@@ -1,5 +1,5 @@
 import factory.*;
-public class Main {
+public class Main3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
