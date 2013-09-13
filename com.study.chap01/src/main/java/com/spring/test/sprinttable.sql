3ab6c0da00d5e016a3a84ff1218d4c1f5b344c6d
sinrim10 <sinrim10@naver.com>
2013-09-12 19:30:55 -0700

    create sql

---
+++ com.study.chap01/src/main/java/com/spring/test/sprinttable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `springtable` (
+  `seq` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `sex` varchar(5) DEFAULT NULL,
+  `addr` varchar(30) DEFAULT NULL,
+  `tellphone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`seq`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=euckr;
+
